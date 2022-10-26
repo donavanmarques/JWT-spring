8f604f9175910617028b07734a247c607f7b11a5
donavanmarques <donavan.fm@hotmail.com>
2022-10-26 20:52:45 -0300

    agr foi

---
+++ jwt/src/main/java/com/Donavan/jwt/model/JwtResponse.java
@@ -0,0 +1,13 @@
+package com.Donavan.jwt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+
+    private String jwtToken;
+}
